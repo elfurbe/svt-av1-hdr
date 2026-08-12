bca4b80b35b24e919459be4d8e869dd72bd1a308
Adam Michel <elfurbe@furbism.com>
2026-08-11 22:55:49 -0700

    Use a 4k file for profiling instead

--- Build/obj1fastdownloader.cmake
+++ Build/obj1fastdownloader.cmake
@@ -10,10 +10,10 @@ set(BUILD_DIRECTORY ${CMAKE_ARGV3})
 # We only need to encode one short video during profiling, in order to execute
 # the code paths we want to optimise. Encoding 29 more files provides no
 # benefits and slows down the process to an unacceptable level.
-set(PROFILING_VIDEO "stefan_sif.y4m")
+set(PROFILING_VIDEO "Netflix_FoodMarket2_4096x2160_60fps_8bit_420.y4m")
 
 set(EXPECTED_HASH
-    28c0dc1afdd9081d8004cc35f839f8d34cabc0fc8b677f9f70286be6bdbd9f57)
+    5ec3b0037a3f74d911dcacf19483535279549658dbef621eb8c0437683360f95)
 set(FILEHASH "")
 if(EXISTS ${BUILD_DIRECTORY}/objective-1-fast/${PROFILING_VIDEO})
     file(SHA256 "${BUILD_DIRECTORY}/objective-1-fast/${PROFILING_VIDEO}" FILEHASH)
